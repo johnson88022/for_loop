3bfc29ed03b71e60c69f7471174b0e806fa24ef1
johnson88022 <johnson88022@gmail.com>
2021-12-22 16:35:49 +0800

    list_2

---
+++ list_2.py
@@ -0,0 +1,13 @@
+# list 清單
+
+a = ["letou","Mrcat"] #第1個索引值為0
+print(a)
+print(a[0])	#列印a清單的第1個索引值
+print(a[1])	#列印a清單的第2個索引值
+
+a.append("Vwin") #將Vwin加入a清單
+print(a)
+
+print(len(a))	#列印出a清單的length長度
+
+print("Vwin" in a) #檢查Vwin是否在a清單內(是非題,True or False)
