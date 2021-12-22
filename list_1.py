7b97300fcb1d2c4682e65d6caf6cd47ebb204374
johnson88022 <johnson88022@gmail.com>
2021-12-22 17:11:44 +0800

    list_1-1

--- list_1.py
+++ list_1.py
@@ -1,3 +1,16 @@
-name = ["Allen","Tom","Mayday","JJ","Jolin","Jay","Jam"]
-for hihi in name:
-	print("Hi! ",hihi,"!")
+# list 清單
+
+a = ["letou","Mrcat"] #第1個索引值為0
+print(a)
+print(a[0])	#列印a清單的第1個索引值
+print(a[1])	#列印a清單的第2個索引值
+
+a.append("Vwin") #將Vwin加入a清單
+print(a)
+
+print(len(a))	#列印出a清單的length(長度)
+
+print("Vwin" in a) #檢查Vwin是否在a清單內(是非題,True or False)
+
+
+
